6690cb9f6786142cba73c9b4869e545cce90b5a7
mkvardas <46765727+mkvardas@users.noreply.github.com>
2019-02-19 05:25:29 +0000

    Bunch of QOL changes
    -added process for getting OMIM ids and filtering nodes based on any shared id with query
    -added process for generating checkboxes based on query OMIM IDs that can filter nodes based on that OMIM id alone
    -added button & function for hiding/showing the aforementioned list of individual query ID filters
    -added context menu choices for downloading network as PNG or JPG (these work on background only, not on selected edges or nodes)
    -added context menu for downloading network as json text file => this or similar will be processed into csv by steps pending to be added
    -cleaned up HTML by redirecting checkbox functions to a function that chooses optionfilter type based on dropdown menu choice
    
    -some quality of life changes (e.g. html says when GO/OMIM loading complete or if no query GO/OMIM terms are found,  popup preventing link to phyrerisk page if node is non-human etc.)
    
    
    
    Attempted to make bot script that fetches OMIM id values (disease) through search engine results (since OMIM prevents html crawling, forcing the user to request an API key), but search engine results don't show full names of an OMIM entry. Trash leftover code is included, keeping in case something similar is needed).

--- protquery.js
+++ protquery.js
@@ -28,18 +28,20 @@ function BuildNetwork() {
 function fetchAll(query) {
 // Offspring and new edges need to be reset with each iteration
 var offspring = [];  // New nodes that will be queried in next iteration
-var saved = [];  // Elements saved in memory in case they need to be added
-Promise.all(query.map(id => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/interaction-min/"+id+".json")
+var edges = [];  // Edges that are saved in memory in case they need to be added
+var nonhuman = [];
+Promise.all(query.map(id => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/interaction-min/" + id + ".json")
 .then(res => res.json())
 .then(function(data) {
-  ids.push(id);
-
+    
   // Retrieve gene name, protein name, organism
   var name = data.entryName.replace("_HUMAN", "");
+
   var fullName = data.recommendedName;
 
   var GO = {"F":[], "P":[], "C":[]};  
-  
+  var OMIM = [];
+  var commonOMIM;
   var structures = [];
   var phyremodels = [];
   
@@ -55,24 +57,16 @@ Promise.all(query.map(id => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/intera
       phyremodels.push(phyremodel);
   }
 
-  // Push node to elements with relevant information
-  elements.push({data: {
-    id: id, 
-    name: name,
-    fullName: fullName,
-    GO: GO,
-    structures: structures,
-    phyremodels: phyremodels,
-    commonGO: {},
-  }});
-
   // Retrieve interactors
   var interactors = data.interactor;
 
   for(var i=0; i<interactors.length; i++) {
     if (!interactors[i].accession) {
       if (interactors[i].intactId2 == data.intactId1) {
-        elements.push({data: {source: id, target: id}});
+        elements.push({data: {
+          source: id,
+          target: id
+      }});
       }
       continue
     }
@@ -82,43 +76,59 @@ Promise.all(query.map(id => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/intera
     if(!ignore[id].includes(interactor)
        && !flagged.includes(interactor)) {
 
-      // Generate edge
-      var edge = {data: {source: id, target: interactor}};
+      // Push edge to array for later use
+      edges.push({data: {
+        source: id, 
+        target: interactor,
+      }});
 
       if (!ids.includes(interactor)) {
+        ids.push(interactor);
+        // Ignore previously encountered binary interactions
+        ignore[interactor] = [id];
+
         if (interactors[i].organismDiffers) {
           // Non-human protein won't have a database page
-          // Therefore node and edge immediately pushed with available info
+          // Therefore a node is pushed with the available information
 
-          elements.push({data: {
+          edges.push({data: {
             id: interactor,
             name: altName(interactors[i].label, interactor).toLowerCase(),
             fullName: altName(interactors[i].recommededName, "(Non-human)"),
             organismDiffers: true,
             GO: GO,
+            OMIM: OMIM,
             structures: [],
             phyremodels: [],
             commonGO: {},
+            commonOMIM: []
           }});
-          elements.push(edge);
-
-        } else {  // If organism is human
-          saved.push(edge); // Save edge to interactor (has no node yet)
-
-          // Ignore previously encountered binary interactions
-          if(Object.keys(ignore).includes(interactor)) {
-            ignore[interactor].push(id);
-          } else {
-            offspring.push(interactor); // Prepare to query interactor
-            ignore[interactor] = [id];
-          }
         }
-      } else { // If protein has already been queried and node exists
-        elements.push(edge); // interactor already has a node so add edge
+
+        else {
+          // Prepare to query interactor itself in next iteration
+          offspring.push(interactor); 
+        }
+
+      }
+      else {
         ignore[interactor].push(id);
       }
     }
   }
+
+  // Push node to elements with relevant information
+  elements.push({data: {
+    id: id, 
+    name: name,
+    fullName: fullName,
+    GO: GO,
+    OMIM: OMIM,
+    structures: structures,
+    phyremodels: phyremodels,
+    commonGO: {},
+    commonOMIM: []
+  }});
 })
 .catch(function() {
   // If error is encountered for initial query, submitted ID is likely invalid
@@ -140,7 +150,8 @@ Promise.all(query.map(id => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/intera
 .then(function(){
 // End recursion if the next iteration needs to query >= 200 interactors
 if (offspring.length < 200) {
-  elements = elements.concat(saved);
+  elements = elements.concat(edges);
+  elements = elements.concat(nonhuman);
   fetchAll(offspring);
 }
 
@@ -179,20 +190,30 @@ cy = cytoscape({
 });
 console.timeEnd("layout");
 
+
 // Colour query node 
 queryNode = cy.nodes()[0];
 queryNode.style({"background-color": "red"});
 
+
 // Style loop edges for self-interactions
 cy.edges(":loop").style("loop-direction", -90);
 
+
 // Once network is rendered, display settings
 document.getElementById("dropdown").style.display = "block";
-document.getElementById("settings1").style.display = "block";
+document.getElementById("settings").style.display = "block";
+document.getElementById("extraOMIM").disabled = true;
+document.getElementById("extracheckboxes").style.display = "none";
+OMIMcheckboxes = document.getElementsByClassName("OMIMcheck")
+for (var x=0; x<OMIMcheckboxes.length; x++){
+    OMIMcheckboxes[x].disabled = true;
+}
 GOcheckboxes = document.getElementsByClassName("GOcheck")
 for (var x=0; x<GOcheckboxes.length; x++){
     GOcheckboxes[x].disabled = true;
 }
+
 
 // Add classes for non-human nodes and nodes without 3D structures
 for (var i=1; i<cy.nodes().length; i++) {
@@ -204,12 +225,111 @@ for (var i=1; i<cy.nodes().length; i++) {
   }
 }
 
-// Work out shared GO terms between all nodes and query (root) node
+
+// Get OMIM IDs and work out shared IDs between all nodes and query (root) node
+console.time("OMIM")
+document.getElementById("loadingOMIM").innerHTML = "Loading OMIM IDs..." 
+Promise.all(cy.nodes().map(node => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/dbref/"+node.data("id").substring(0, 6)+"/MIM.json") // GO API has no page on isoform GO terms, therefore use canonical GOs. Uniprot is the same
+.then(response => response.json())
+.then(function (OMIMids) { 
+    if (OMIMids && OMIMids.length != 0){
+    for (var i=0; i<OMIMids.length; i++) {
+      
+      if (OMIMids[i].properties.type == "phenotype"){
+      var id = OMIMids[i].id
+      node.data("OMIM").push(id);
+      }
+     }
+    }
+    else {
+        node.data({
+            OMIM: []
+        })
+    }
+})))
+.then(function(){
+if (queryNode.data("OMIM").length != 0) {
+    for (var i=0; i<cy.nodes().length; i++){
+        var queryOMIM = queryNode.data("OMIM")
+        var targetOMIM = cy.nodes()[i].data("OMIM")
+        var intersect = targetOMIM.filter(value => -1 !== queryOMIM.indexOf(value))
+        if (intersect.length == 0){
+            cy.nodes()[i].addClass("rejectOMIM")
+            cy.nodes()[i].data("commonOMIM", "none")
+        }
+        
+        else {
+            cy.nodes()[i].data("commonOMIM", intersect)
+        
+    }
+}
+var div = document.getElementById('extracheckboxes')
+    for (var z=0; z<queryNode.data("commonOMIM").length; z++) {                 // Loop for adding checkboxes to HTML to filter for each query OMIM ID 
+        var term = queryNode.data("commonOMIM")[z]
+        if (term != "none") {
+            div.innerHTML += `
+            <tr>
+            <td>Query ID: ` + term  + `</td>
+            <td><input class="OMIMcheck" id="OMIMcheck" type="CHECKBOX" value="1" onchange="Optionfilterchoice(this, '.reject`+ term + `');"/></td>
+            </tr>
+            `    
+            
+            for (var i=1; i<cy.nodes().length; i++){                                            
+                var targetOMIM = cy.nodes()[i].data("commonOMIM")                       // Loop within previous loop for adding individual OMIM term reject classes to each node
+                if (targetOMIM.indexOf(term) == -1) {
+                    cy.nodes()[i].addClass("reject" + term)
+                }
+            }
+        }
+    }
+   document.getElementById("loadingOMIM").innerHTML = "Loading OMIM IDs... COMPLETE" 
+}
+else {
+   document.getElementById("loadingOMIM").innerHTML = "No OMIM IDs found for query"
+}
+
+document.getElementById("extraOMIM").disabled = false;
+for (var x=0; x<OMIMcheckboxes.length; x++){
+    OMIMcheckboxes[x].disabled = false;
+}
+
+
+
+console.timeEnd("OMIM")
+})
+
+
+ // OMIM permabans IPs that use bots to mine information from their htmls such as OMIM id values ( https://omim.org/robots.txt ), so need to request API key to access its information
+ 
+ // Follow-up script that scrapes search engine results for the value of each node's shared OMIM id. Used bing because it allows bots to scrape its results, unlike google. Discarded because engines truncate full OMIM entry titles.
+ 
+ /*       .then(function(){
+        for (var x=0; x<cy.nodes().length; x++){
+            if (cy.nodes()[x].data("commonOMIM").length > 0 && cy.nodes()[x].data("commonOMIM").indexOf("none") == -1) {
+                console.log(cy.nodes()[x].data("commonOMIM"))
+                for (var i=0; i<cy.nodes()[x].data("commonOMIM").length; i++){
+                OMIMid = cy.nodes()[x].data("commonOMIM")[i]
+                fetch("https://cors-anywhere.herokuapp.com/https://www.bing.com/search?q=omim+" + OMIMid)
+          .then(response => response.text())
+          .then(text => {
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(text, "text/html");
+            const result1 = htmlDocument.documentElement.querySelector("h2 a").innerText;
+            cy.nodes()[x].data("commonOMIM")[i] = result1
+          })
+                   
+                }
+            }
+        }
+    })
+ */
+
+
+// Get GO terms and work out shared GO terms between all nodes and query (root) node
 console.time("intersection");
 
-document.getElementById("loading").innerHTML = "Loading GO terms..." 
-Promise.all(cy.nodes().map(node => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/dbref/"+node.data("id").substring(0, 6)+"/GO.json") 
-// GO API has no page on isoform GO terms, therefore use canonical GOs. Uniprot is the same
+document.getElementById("loadingGO").innerHTML = "Loading GO terms..." 
+Promise.all(cy.nodes().map(node => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest/dbref/"+node.data("id").substring(0, 6)+"/GO.json") // GO API has no page on isoform GO terms, therefore use canonical GOs. Uniprot is the same
 .then(response => response.json())
 .then(function (GOterms) { 
     if (GOterms && GOterms.length != 0){
@@ -225,28 +345,33 @@ Promise.all(cy.nodes().map(node => fetch("http://phyrerisk.bc.ic.ac.uk:9090/rest
     }
 })))
 .then(function(){
+if (Object.values(queryNode.data("GO")).length != 0) {
     for (var i=0; i<3; i++) {  // Loop through categories
-    for (var j=1; j<cy.nodes().length; j++) {  // Loop through nodes excluding query
-    var queryGO = queryNode.data("GO")[categories[i]];
-    var targetGO = cy.nodes()[j].data("GO")[categories[i]];
-    var intersect = targetGO.filter(value => -1 !== queryGO.indexOf(value))
-    
-    if (intersect.length == 0) {
-      cy.nodes()[j].addClass("reject" + categories[i]);
-      cy.nodes()[j].data("commonGO")[categories[i]] = "none";
-    }
+        for (var j=1; j<cy.nodes().length; j++) {  // Loop through nodes excluding query
+        var queryGO = queryNode.data("GO")[categories[i]];
+        var targetGO = cy.nodes()[j].data("GO")[categories[i]];
+        var intersect = targetGO.filter(value => -1 !== queryGO.indexOf(value))
+        
+        if (intersect.length == 0) {
+          cy.nodes()[j].addClass("reject" + categories[i]);
+          cy.nodes()[j].data("commonGO")[categories[i]] = "none";
+        }
 
-    else {
-      cy.nodes()[j].data("commonGO")[categories[i]] = intersect.join(", ");
-    }
-  }
-} 
+        else {
+          cy.nodes()[j].data("commonGO")[categories[i]] = intersect.join(", ");
+        }
+     }
+   }     
+document.getElementById("loadingGO").innerHTML = "Loading GO terms... COMPLETE" 
+}
+else {document.getElementById("loadingGO").innerHTML = "No GO terms found for query"}
 console.timeEnd("intersection");
 for (var x=0; x<GOcheckboxes.length; x++){
     GOcheckboxes[x].disabled = false;
 }
-document.getElementById("loading").innerHTML = "" 
+
 })
+
 
 
 // Define on-click, on-mouseover etc. events
@@ -283,6 +408,7 @@ cy.on("layoutstop", function(){
   cy.panBy({x:-240, y:-35});
 });
 
+
 // Define right-click context menu
 var contextMenu = cy.contextMenus({
   menuItems: [
@@ -292,16 +418,18 @@ var contextMenu = cy.contextMenus({
       selector: "node",
       onClickFunction: function (event) {
       var target = event.target || event.cyTarget;
-
-      // Add "-1" to end of link if isoform isn"t specified
-      // This is because pages without a specified isoform lack some information
-      if (target.data("id").length == 6){
-          var id = target.data("id") + "-1";
-      }
-      else { 
-        var id = target.data("id")}
-        window.open("http://phyrerisk.bc.ic.ac.uk:8080/isoform/"+id);
-      },
+      if (!target.data("organismDiffers")) {
+          // Add "-1" to end of link if isoform isn"t specified
+          // This is because pages without a specified isoform lack some information
+          if (target.data("id").length == 6){
+              var id = target.data("id") + "-1";
+          }
+          else { 
+            var id = target.data("id")}
+            window.open("http://phyrerisk.bc.ic.ac.uk:8080/isoform/"+id);
+       }
+       else {alert("This protein is non-human and thus not in the PhyreRisk database.")}
+    },
       hasTrailingDivider: true
     },
     {
@@ -309,30 +437,79 @@ var contextMenu = cy.contextMenus({
       content: "Show Gene Ontology features shared with query",
       selector: "node",
       onClickFunction: function (event) {
-        var target = event.target || event.cyTarget;
-        alert(["Shared cellular component:\n" + target.data("commonGO").C,
-               "Shared biological process:\n" + target.data("commonGO").P,
-               "Shared molecular function:\n" + target.data("commonGO").F]
-               .join("\n\n"));
-        },
+        if (Object.values(queryNode.data("GO")).length != 0) {
+            var target = event.target || event.cyTarget;
+            if (document.getElementById("loadingGO").innerHTML == "" ) {
+                alert(["Shared cellular component:\n" + target.data("commonGO").C,
+                       "Shared biological process:\n" + target.data("commonGO").P,
+                       "Shared molecular function:\n" + target.data("commonGO").F]
+                       .join("\n\n"));
+                }
+             else {alert("GO terms still loading...")}
+        }
+        else {alert("No GO terms found for query")}
+    },
       hasTrailingDivider: true
     },
-    // The next two buttons are currently unused
     {
       id: "Diseaseinvolement",
       content: "Show disease involvement shared with query",
       selector: "node",
       onClickFunction: function (event) {
-        var target = event.target || event.cyTarget;
+        if (queryNode.data("OMIM").length != 0) {
+            var target = event.target || event.cyTarget;
+            if (document.getElementById("loadingOMIM").innerHTML == "" ) {
+                alert("Shared OMIM disease involvement:\n" + target.data("commonOMIM"));
+                }
+            else {alert("OMIM IDs still loading...")}    
+        }
+        else {alert("No OMIM IDs found for query")}
+    },
+      hasTrailingDivider: true
+    },
+    {
+      id: "excel",
+      content: "Export network as Excel table",
+      coreAsWell: true,
+      onClickFunction: function () {
+        json = cy.json()
+        text = JSON.stringify(json)                                     // Need to add steps here to process file into csv format
+        var a = document.createElement('a');
+        a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));    
+        a.setAttribute("download",  "HELP! I'm trapped inside this json")
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
       },
       hasTrailingDivider: true
     },
     {
-      id: "Structure",
-      content: "Show protein 3D structure",
-      selector: "node",
-      onClickFunction: function (event) {
-        var target = event.target || event.cyTarget;
+      id: "jpg",
+      content: "Export network as JPG image",
+      coreAsWell: true,
+      onClickFunction: function () {
+        image = cy.jpg()
+        var a = document.createElement('a');
+        a.href = image
+        a.setAttribute("download",  "HELP! I'm trapped inside this jpg")
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      },
+      hasTrailingDivider: true
+    },
+    {
+      id: "png",
+      content: "Export network as PNG image",
+      coreAsWell: true,
+      onClickFunction: function () {
+        image = cy.png()
+        var a = document.createElement('a');
+        a.href = image
+        a.setAttribute("download",  "HELP! I'm trapped inside this png")
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
       },
       hasTrailingDivider: true
     }
@@ -340,17 +517,46 @@ var contextMenu = cy.contextMenus({
 });
 }})}
 
-//Display filtering method based on drop-down menu choice
 
-function DisplaySettings(method){
-    document.getElementById("settings1").style.display = "none";
-    document.getElementById("settings2").style.display = "none";
-    document.getElementById(method).style.display = "block";
-    
+// Display individual OMIM ID filters
+function showextraOMIM(){
+    var state = document.getElementById("extraOMIM").innerHTML
+    var div = document.getElementById('extracheckboxes')
+    if (state == "Show"){
+        document.getElementById("extracheckboxes").style.display = "table-footer-group";
+        document.getElementById("extraOMIM").innerHTML = "Hide"
+    }
+    else if (state == "Hide") {
+        document.getElementById("extracheckboxes").style.display = "none";
+        document.getElementById("extraOMIM").innerHTML = "Show"
+    }
 }
 
 
-//Define filtering functions for each method
+// Display filtering method based on drop-down menu choice
+
+var dropdownchoice;                         
+dropdownchoice = "method1"                  // Default setting
+function DisplaySettings(method){
+    dropdownchoice = method
+}
+
+
+
+// Tell html which filtering function to use based on dropdown choice
+
+function Optionfilterchoice(checkBoxID, optionclass){
+    if (dropdownchoice == "method1") {
+        Optionfilter(checkBoxID, optionclass)
+    }
+    else  if (dropdownchoice == "method2") {
+        OptionfilterV2(checkBoxID, optionclass)
+    }
+}
+
+
+
+// Define filtering functions for each method
 
 function Optionfilter(checkBoxID, optionClass) {
   if (checkBoxID.checked){
@@ -383,8 +589,8 @@ function Optionfilter(checkBoxID, optionClass) {
 }  
      
 
-function OptionfilterV2(checkboxid, optionclass) {
- if (checkboxid.checked){
+function OptionfilterV2(checkBoxID, optionclass) {
+ if (checkBoxID.checked){
      
      checkEvents2.push(optionclass)
      cy.$(optionclass).style("opacity", 0);
@@ -417,6 +623,8 @@ function OptionfilterV2(checkboxid, optionclass) {
     }
 }
 }
+
+  
 
 // Define node collapse and expansion functions
 
